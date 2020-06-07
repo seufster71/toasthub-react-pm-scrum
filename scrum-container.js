9a86acde0cd19557e202ebdb9e2260ff60969818
Edward Seufert <edwardseufert@Edwards-MBP.home>
2020-06-07 12:46:59 -0400

    pm buildout

--- scrum-container.js
+++ scrum-container.js
@@ -177,7 +177,7 @@ class PMScrumContainer extends Component {
 				id = item.id;
 			}
 			fuLogger.log({level:'TRACE',loc:'ScrumContainer::onModify',msg:"test"+id});
-			this.props.actions.modifyItem(id);
+			this.props.actions.modifyItem({id,appPrefs:this.props.appPrefs});
 		};
 	}
 	
